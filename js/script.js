e24ad2fbb0e8a59d641f54983f3c50492342b6f8
MayaSelena773 <maya.bono721@gmail.com>
2022-06-23 15:57:22 +0200

    title

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+var app = new Vue(
+    {
+
+    el: '#root',
+    data: {
+            title:'Benvenuto Vuejs :)'
+    }
+
+}
+
+
+)
